77053e37bda4d37e0768e366e05f03ba5a2582c5
Alex <arkanjo.alex@gmail.com>
2019-04-25 08:26:20 -0300

    teste OK

--- University/dr2-java-fund-prog/week2/week2-aula1/src/exercise/Retangulo.java
+++ University/dr2-java-fund-prog/week2/week2-aula1/src/exercise/Retangulo.java
@@ -4,6 +4,39 @@ package exercise;
  * Retangulo
  */
 public class Retangulo {
+    private int x;
+    private int y;
+    private int largura;
+    private int altura;
 
+    public Retangulo(int x, int y, int largura, int altura) {        
+        super();
+        this.x = x;
+        this.y = y;
+        this.largura = largura;
+        this.altura = altura;
+    }
     
+    public void mover(float sx, float sy) {
+        
+    }
+
+    public void desenhar() {
+        
+    }
+
+    public void redimensionar() {
+        
+    }
+
+    public String toString() {
+        
+
+        return "";
+    }
+
+    public void printTest() {
+        System.out.println("Ta OK!");
+        
+    }
 }
