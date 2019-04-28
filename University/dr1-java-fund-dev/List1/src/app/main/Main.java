10ad6da06943a80744bc29932a24e8767ff6492b
Alex Santos <arkanjo.alex@gmail.com>
2019-04-28 14:05:37 -0300

    REPLACE dr1-lista1

---
+++ University/dr1-java-fund-dev/List1/src/app/main/Main.java
@@ -0,0 +1,18 @@
+package app.main;
+
+//RUN THIS FILE: Ctrl + Shift + F10
+public class Main {
+
+    public static void main(String[] args) {
+        //MainEx1.executeEx();
+        //MainEx2.executeEx();
+        //MainEx3.executeEx();
+        //MainEx4.executeEx();
+        //MainEx5.executeEx();
+        //MainEx6.executeEx();
+        //MainEx7.executeEx();
+        //MainEx8.executeEx();
+        //MainEx9.executeEx();
+        //MainEx10.executeEx();
+    }
+}
