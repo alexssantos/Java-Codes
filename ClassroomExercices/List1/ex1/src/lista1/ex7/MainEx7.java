991f8f9a437662ddfcbdcd06e57d033bd8865596
aarkanjo <arkanjo.alex@gmail.com>
2019-04-28 13:17:13 -0300

    Exercise 7 - DONE

--- ClassroomExercices/List1/ex1/src/lista1/ex7/MainEx7.java
+++ ClassroomExercices/List1/ex1/src/lista1/ex7/MainEx7.java
@@ -1,8 +1,8 @@
-package lista1.ex6;
+package lista1.ex7;
 
 import java.util.Scanner;
 
-public class MainEx6 {
+public class MainEx7 {
     public static void main(String[] args) {
         executeEx();
     }
@@ -11,13 +11,15 @@ public class MainEx6 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("CALCULO DA AREA DO QUADRADO!");
-        System.out.print("Digite o valor do lado: ");
-        double side = Double.parseDouble(sc.next());
+        System.out.print("Digite o valor da altura: ");
+        double height = Double.parseDouble(sc.next());
+        System.out.print("Digite o valor da base: ");
+        double base = Double.parseDouble(sc.next());
 
         sc.close();
 
-        //Area = L^2
-        double area = Math.pow(side, 2);
+        //Area = b * h
+        double area = height * base;
 
         System.out.println("AREA: " + area +" m²");
     }
