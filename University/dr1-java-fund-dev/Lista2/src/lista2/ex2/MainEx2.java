55e55c867a600e482968db612c7fb13acd142560
Alex Santos <arkanjo.alex@gmail.com>
2019-04-28 15:55:55 -0300

    Lista 2 - empty base classes

---
+++ University/dr1-java-fund-dev/Lista2/src/lista2/ex2/MainEx2.java
@@ -0,0 +1,12 @@
+package lista2.ex2;
+
+public class MainEx2 {
+
+    public static void main(String[] args) {
+        executeEx();
+    }
+
+    public static void executeEx(){
+
+    }
+}
---
+++ University/dr1-java-fund-dev/Lista2/src/lista2/ex6/MainEx6.java
@@ -0,0 +1,12 @@
+package lista2.ex6;
+
+public class MainEx6 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void executeEx(){
+
+    }
+}
