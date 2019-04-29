02ee808f74aa499093bfa62da64b07d5ab136e64
Alex <arkanjo.alex@gmail.com>
2019-04-29 09:07:47 -0300

    DR1-TP1: FIX

--- University/dr1-java-fund-dev/tp1/src/alex/silva/Main.java
+++ University/dr1-java-fund-dev/tp1/src/alex/silva/Main.java
@@ -9,9 +9,6 @@ public class Main {
         String numb1, numb2;
         String[] operations = {"+","-","/","*"};
 
-        numb1 = JOptionPane.showInputDialog(null, "Digite o Primeiro valor: ");
-        double num1 =Double.parseDouble(numb1);
-
         String choice = (String) JOptionPane.showInputDialog(null,
                 "Operação:",            //msg
                 "Escolha da operação.",     //title
@@ -19,6 +16,9 @@ public class Main {
                 null,          // icon
                 operations,         // Array of choices
                 operations[1]);     // Initial choice
+
+        numb1 = JOptionPane.showInputDialog(null, "Digite o Primeiro valor: ");
+        double num1 =Double.parseDouble(numb1);
 
         numb2 = JOptionPane.showInputDialog(null, "Digite o segundo valor: ");
         double num2 =Double.parseDouble(numb2);
@@ -29,17 +29,17 @@ public class Main {
             result = num1 + num2;
             msg("Resultado: ", result);
         }
-        if (choice.equals("-"))
+        else if (choice.equals("-"))
         {
             result = num1 - num2;
             msg("Resultado: ", result);
         }
-        if (choice.equals("*"))
+        else if (choice.equals("*"))
         {
             result = num1 * num2;
             msg("Resultado: ", result);
         }
-        if (choice.equals("/"))
+        else if (choice.equals("/"))
         {
             if ((num2 == 0) || (num1 == 0)){
                 msg("Alerta! Não é possível fazer a divisão por '0' ", null);
@@ -58,6 +58,5 @@ public class Main {
         else{
             JOptionPane.showMessageDialog(null,msg + result);
         }
-
     };
 }
