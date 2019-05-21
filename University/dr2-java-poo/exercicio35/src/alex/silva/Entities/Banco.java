2496c3953aeec01200fd897bf98b924d129f3913
Alex <arkanjo.alex@gmail.com>
2019-05-21 08:56:33 -0300

    Exercise 37 - progress

--- University/dr2-java-poo/exercicio35/src/alex/silva/Entities/Banco.java
+++ University/dr2-java-poo/exercicio35/src/alex/silva/Entities/Banco.java
@@ -1,19 +1,84 @@
 package alex.silva.Entities;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
     private int codigo;
     private String nome;
     private Conta contas[];
+    private int qtdMaxContas = 0;
+    private int qtdContas = 0;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public Banco(int codigo, String nome, int qtdMaxContas) {
         this.codigo = codigo;
         this.nome = nome;
         this.contas = new Conta[qtdMaxContas];
+        this.qtdMaxContas = qtdMaxContas;
     }
 
-    
+    public void addConta(Conta newConta){
+        if (qtdContas >= qtdMaxContas){return;}
+
+        if (newConta instanceof ContaPoupanca){
+            this.contas[qtdContas] = newConta;
+        }
+        if (newConta instanceof ContaComum){
+
+        }
+        if (newConta instanceof ContaEspecial){
+
+        }
+        qtdContas++;
+    }
+
+    public void setManyContas(List<Conta> contas){
+        for (Conta conta: contas) {
+            if (conta != null){
+                addConta(conta);
+            }
+        }
+    }
+
+    public void deleteConta(int numeroConta){
+        for (int i=0; i < qtdMaxContas; i++){
+            if (this.contas[i].getContaNumero() == numeroConta){
+                contas[i] = contas[qtdContas--];
+                qtdContas--;
+                contas[qtdContas--] = null;      //remove duplicate scraps
+            }
+        }
+    }
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void listar(){
+
+    }
+
+    public void imprimirTotal(){
+        //Imprime soma de todas as contas
+    }
 
 }
