657b59ba125bddcce28eba09b711bc43e9f36bff
Alex <arkanjo.alex@gmail.com>
2019-06-03 08:22:12 -0300

    TP3 - start + CANCEL option

---
+++ University/dr1-java-fund-dev/tp3/src/alex/silva/ICalculator.java
@@ -0,0 +1,13 @@
+package alex.silva;
+
+public interface ICalculator {
+
+    public Double soma(Double num1, Double num2);
+
+    public Double subtracao(Double num1, Double num2);
+
+    public Double multiplicacao(Double num1, Double num2);
+
+    public Double divisao(Double num1, Double num2);
+
+}
