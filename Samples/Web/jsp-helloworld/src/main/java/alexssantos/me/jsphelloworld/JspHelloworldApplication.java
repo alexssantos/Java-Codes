4b157f893d568532638346be26ec7910fbeb11cd
Alex Santos <arkanjo.alex@gmail.com>
2019-09-24 08:16:03 -0300

    SAMPLE NEW - WEB - JSP Hello World

---
+++ Samples/Web/jsp-helloworld/src/main/java/alexssantos/me/jsphelloworld/JspHelloworldApplication.java
@@ -0,0 +1,13 @@
+package alexssantos.me.jsphelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspHelloworldApplication.class, args);
+    }
+
+}
