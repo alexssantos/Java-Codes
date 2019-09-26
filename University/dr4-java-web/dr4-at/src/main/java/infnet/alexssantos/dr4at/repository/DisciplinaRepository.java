6bdc2936f389d83802fd088cd552d611d6fa632c
Alex Santos <arkanjo.alex@gmail.com>
2019-09-26 08:37:33 -0300

    AT DR4 - Java Web
    Repositorios Criados para as Entidades.

--- University/dr4-java-web/dr4-at/src/main/java/infnet/alexssantos/dr4at/repository/DisciplinaRepository.java
+++ University/dr4-java-web/dr4-at/src/main/java/infnet/alexssantos/dr4at/repository/DisciplinaRepository.java
@@ -1,9 +1,9 @@
 package infnet.alexssantos.dr4at.repository;
 
-import infnet.alexssantos.dr4at.model.domain.Usuario;
+import infnet.alexssantos.dr4at.model.domain.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioReposotory extends JpaRepository<Usuario, Integer> {
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Integer> {
 }
--- University/dr4-java-web/dr4-at/src/main/java/infnet/alexssantos/dr4at/repository/PerfilRepository.java
+++ University/dr4-java-web/dr4-at/src/main/java/infnet/alexssantos/dr4at/repository/PerfilRepository.java
@@ -1,9 +1,9 @@
 package infnet.alexssantos.dr4at.repository;
 
-import infnet.alexssantos.dr4at.model.domain.Usuario;
+import infnet.alexssantos.dr4at.model.domain.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioReposotory extends JpaRepository<Usuario, Integer> {
+public interface PerfilRepository extends JpaRepository<Perfil, Integer> {
 }
