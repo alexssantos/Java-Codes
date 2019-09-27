b934273985a6840ac1739228f5fc245efb727620
Alex Santos <arkanjo.alex@gmail.com>
2019-09-26 23:12:21 -0300

    AT DR4 - progress

--- University/dr4-java-web/dr4-at/src/main/java/infnet/alexssantos/dr4at/repository/AlunoRepository.java
+++ University/dr4-java-web/dr4-at/src/main/java/infnet/alexssantos/dr4at/repository/AlunoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
+public interface AlunoRepo extends JpaRepository<Aluno, Integer> {
 
-    public Aluno getAlunoByUsuario(Usuario usuario);
+    //public Aluno getAlunoByUsuario(Usuario usuario);
 }
